83f30ee106487d456daef8bd32282f4b51d6538e
Divyanshu Parihar <divyanshu1447@gmail.com>
2022-11-22 22:46:33 +0530

    prod

--- commands/calc.js
+++ commands/calc.js
@@ -1,47 +1,60 @@
-const { SlashCommandBuilder } = require('discord.js');
-const { ActionRowBuilder,  ModalBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
+const { SlashCommandBuilder, SelectMenuComponent } = require('discord.js');
+const { ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, SelectMenuBuilder } = require('discord.js');
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('calc')
-		.setDescription('calculate the profit and loss with bets'),
-	async execute(interaction) {
+    data: new SlashCommandBuilder()
+        .setName('calc')
+        .setDescription('calculate the profit and loss with bets'),
+    async execute(interaction) {
         // return;
         const modal = new ModalBuilder()
-			.setCustomId('myModal')
-			.setTitle('My Modal');
+            .setCustomId('myModal')
+            .setTitle('Betting Calculator');
 
-		// Add components to modal
+        // Add components to modal
 
-		// Create the text input components
-		const favoriteColorInput = new TextInputBuilder()
-			.setCustomId('firstOdd')
-		    // The label is the prompt the user sees for this input
-			.setLabel("First Odds: ")
-		    // Short means only a single line of text
-			.setStyle(TextInputStyle.Short);
+        // Create the text input components
+        const favoriteColorInput = new TextInputBuilder()
+            .setCustomId('firstOdd')
+            // The label is the prompt the user sees for this input
+            .setLabel("First Odds: ")
+            // Short means only a single line of text
+            .setStyle(TextInputStyle.Short);
 
-		const hobbiesInput = new TextInputBuilder()
-			.setCustomId('secondOdds')
-			.setLabel("Second Odds")
-		    // Paragraph means multiple lines of text.
-			.setStyle(TextInputStyle.Short);
+        const hobbiesInput = new TextInputBuilder()
+            .setCustomId('secondOdds')
+            .setLabel("Second Odds")
+            // Paragraph means multiple lines of text.
+            .setStyle(TextInputStyle.Short);
         const thirdInput = new TextInputBuilder()
-			.setCustomId('thirdOdds')
-			.setLabel("Wager for First Odds")
-		    // Paragraph means multiple lines of text.
-			.setStyle(TextInputStyle.Short);
-            
-            // An action row only holds one text input,
-            // so you need one action row per text input.
-            const firstActionRow = new ActionRowBuilder().addComponents(favoriteColorInput);
-            const secondActionRow = new ActionRowBuilder().addComponents(hobbiesInput);
-            const thirdActionRow = new ActionRowBuilder().addComponents(thirdInput);
-            
-            // Add inputs to the modal
-            modal.addComponents(firstActionRow, secondActionRow,thirdActionRow);
-            
-            // Show the modal to the user
-            return await interaction.showModal(modal)
-            // return interaction.reply('Calculating...');
-        },
-    };
+            .setCustomId('thirdOdds')
+            .setLabel("Wager for First Odds")
+            // Paragraph means multiple lines of text.
+            .setStyle(TextInputStyle.Short);
+
+        // const select = new SelectMenuBuilder().setCustomId('select').setPlaceholder('Positive').addOptions([{
+        //     label: 'Positive',
+        //     description: 'Positive First Odd',
+        //     value: '1',
+        // },
+        // {
+        //     label: 'Negative',
+        //     description: 'Negative First Odd',
+        //     value: '-1',
+        // }])
+        // An action row only holds one text input,
+        // so you need one action row per text input.
+        // const type = new ActionRowBuilder().addComponents(select)
+        const firstActionRow = new ActionRowBuilder().addComponents(favoriteColorInput);
+        const secondActionRow = new ActionRowBuilder().addComponents(hobbiesInput);
+        const thirdActionRow = new ActionRowBuilder().addComponents(thirdInput);
+
+
+        // Add inputs to the modal
+        modal.addComponents(firstActionRow, secondActionRow, thirdActionRow);
+
+
+        // Show the modal to the user
+        return await interaction.showModal(modal)
+        // return interaction.reply('Calculating...');
+    },
+};
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
-const { TOKEN } = require('./config.js');
+const { TOKEN, clientId} = require('./config.js');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -18,59 +18,92 @@ const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	client.commands.set(command.data.name, command);
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    client.commands.set(command.data.name, command);
 }
 
 client.once(Events.ClientReady, () => {
-	console.log('Ready!');
+    console.log('Ready!');
 });
 
 
 client.on(Events.InteractionCreate, interaction => {
-	if (!interaction.isModalSubmit()) return;
-	const firstOdd = interaction.fields.getTextInputValue('firstOdd');
-	const secondOdd = interaction.fields.getTextInputValue('secondOdds');
-	const wager = interaction.fields.getTextInputValue('thirdOdds');
+    if (!interaction.isModalSubmit()) return;
+    const firstOdd = interaction.fields.getTextInputValue('firstOdd');
+    const secondOdd = interaction.fields.getTextInputValue('secondOdds');
+    const wager = interaction.fields.getTextInputValue('thirdOdds');
 
-	console.log({ firstOdd, secondOdd ,wager});
-// $250 on (-256) - Payout 347.66
-// $93.78 on (+270) - Payout 347.66
-// Total Stake - $250 + $93.78 = 343.78
-// Total Profit = 347.66 - $343.78 = $3.88
-    let first = calcProfit(firstOdd,wager)
-    let second = calcProfitWithResult(secondOdd,calcProfit(firstOdd,wager))
+    console.log({ firstOdd, secondOdd, wager });
+    // $250 on (-256) - Payout 347.66
+    // $93.78 on (+270) - Payout 347.66
+    // Total Stake - $250 + $93.78 = 343.78
+    // Total Profit = 347.66 - $343.78 = $3.88
+    let first = calcProfit(firstOdd, wager)
+    let second = calcProfitWithResult(secondOdd, calcProfit(firstOdd, wager))
     let stake = parseFloat(wager) + second
     let profit = first - stake
     return interaction.reply(`$${wager} on (${firstOdd}) - Payout ${first.toFixed(2)}\n$${second.toFixed(2)} on (${secondOdd}) - Payout ${first.toFixed(2)}\nTotal Stake - $${parseFloat(wager).toFixed(2)} + $${second.toFixed(2)} = ${stake.toFixed(2)}\nTotal Profit = $${first.toFixed(2)} - $${stake.toFixed(2)} = $${profit.toFixed(2)}`)
 });
 
-function calcProfit(val,wager){
-    if(val >= 0){
-        return ((Math.abs(val)/100) + 1) * wager;
-    }else {
-        return ((100/Math.abs(val)) + 1 ) * wager;
+function calcProfit(val, wager) {
+    if (val >= 0) {
+        return ((Math.abs(val) / 100) + 1) * wager;
+    } else {
+        return ((100 / Math.abs(val)) + 1) * wager;
     }
 }
 
-function calcProfitWithResult(val,result){
-        return result / ((val/100) + 1)
+function calcProfitWithResult(val, result) {
+    if(val >=0 ) return result / ((Math.abs(val) / 100) + 1)
+    else {
+        return result / (((100/Math.abs(val)) + 1));
+    }
 }
 
+
+client.on(Events.GuildCreate, async (guild) => {
+
+    (async () => {
+        const { REST, Routes } = require('discord.js');
+        const { clientId, TOKEN } = require('./config.js');
+        const fs = require('node:fs');
+        let guildId = guild.id;
+        const commands = [];
+        const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+        for (const file of commandFiles) {
+            const command = require(`./commands/${file}`);
+            commands.push(command.data.toJSON());
+        }
+
+        const rest = new REST({ version: '10' }).setToken(TOKEN);
+        try {
+            console.log(`Started refreshing ${commands.length} application (/) commands.`);
+            const data = await rest.put(
+                Routes.applicationGuildCommands(clientId, guildId),
+                { body: commands },
+            );
+
+            console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+        } catch (error) {
+            console.error(error);
+        }
+    })();
+})
 client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
+    if (!interaction.isChatInputCommand()) return;
 
-	const command = client.commands.get(interaction.commandName);
+    const command = client.commands.get(interaction.commandName);
 
-	if (!command) return;
+    if (!command) return;
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
 });
 
 client.login(TOKEN);
